e6899f959c9c6892ffbf27e3489593dc61138925
chengyan <ab364743113@126.com>
2016-03-07 17:48:06 +0800

    创建项目文件

---
+++ Learning/Learning/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Learning
+//
+//  Created by ChengYan on 16/3/7.
+//  Copyright © 2016年 chengyan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ Learning/lesson1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  lesson1
+//
+//  Created by ChengYan on 16/3/7.
+//  Copyright © 2016年 chengyan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
